3afd05acc2977e9b8daafe3afce8873e8e6ec0c1
bhanucha.boom <bhanucha.boom@loolootech.com>
2026-03-09 13:59:13 +0700

    chore: Add project dependencies and generated build artifacts.

--- backend/wdic/wdic/hangkan/asgi.py
+++ backend/wdic/wdic/hangkan/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hangkan project.
+ASGI config for wdic project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hangkan.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wdic.settings")
 
 application = get_asgi_application()
